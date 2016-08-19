0090b5af3878c0fd6b54cbfa1519ef7621eec550
Tony Mai <tonymaibox@yahoo.com>
2016-08-19 15:55:57 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,15 +4,20 @@ class PostsController < ApplicationController
   end
 
   def edit
+    # binding.pry
     @post = Post.find(params[:id])
   end
 
   def update
     @post = Post.find(params[:id])
+      @post.update(post_params)
+      @new_post = Post.new(post_params)
+    if @new_post.valid?
 
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
